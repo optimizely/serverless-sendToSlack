f098aeff17479d21206a5312d59ce76ad12edcc0
Patrick Gilbert <patrick.gilbert@optimizely.com>
2017-05-10 11:55:54 -0700

    first post, sanitized

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# serverless
+Serverless: https://serverless.com/framework/docs/providers/aws/guide/installation/
+
+## Specifying an S3 Bucket
+It is recommended that you setup a specific bucket in S3 for Serverless to keep the Cloudformation templates in. This will need to be set in your `serverless.yml` file, and must be unique. If you choose not to set a specific bucket (by removing the `deploymentBucket` key), a new one will be created on every deploy.
+
+## Vendoring Dependencies
+
+To install python package dependencies (`requirements.txt`) use the included
+`build.sh` script:
+```console
+$ ./build.sh
+```
+
+This runs an `amazonlinux` docker container, matching the environment of AWS
+Lambda, ensuring dependencies are compiled to the correct OS.
+
+These dependencies won't work when invoking the function locally if you're
+developing on OSX. In that case you should create a virtualenv and install the
+dependencies manually:
+
+```console
+$ virtualenv env
+$ source env/bin/activate
+$ pip install -r requirements.txt
+```
+
+## Deployment
+
+Start by (Vendoring Dependencies)[#vendoring-dependencies], then install
+serverless:
+```console
+$ npm install -g serverless
+```
+
+Deploy the function with:
+```console
+$ serverless deploy
+```
---
+++ sendtoslack/sendToSlack.py
@@ -0,0 +1,58 @@
+'''
+This has been customized from the original version in AWS Lambda's pre-made script library.
+
+Follow these steps to configure the webhook in Slack:
+
+  1. Navigate to https://<your-team-domain>.slack.com/services/new
+
+  2. Search for and select "Incoming WebHooks".
+
+  3. Choose the default channel where messages will be sent and click "Add Incoming WebHooks Integration".
+
+  4. Copy the webhook URL from the setup instructions and use it in the next section.
+'''
+from __future__ import print_function
+
+import os
+import sys
+
+# Only load linux-compiled dependencies if executing under linux
+if sys.platform.startswith('linux'):
+  here = os.path.dirname(os.path.realpath(__file__))
+  sys.path.append(os.path.join(here, "vendored"))
+
+import boto3
+import datetime
+import json
+import logging
+
+from envparse import env
+from urllib2 import Request, urlopen, URLError, HTTPError
+
+# The Slack channel to send a message to stored in the slackChannel environment variable
+SLACK_CHANNEL = env('SLACK_CHANNEL')
+HOOK_URL = env('SLACK_HOOK_URL')
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+json.JSONEncoder.default = lambda self, obj: (obj.isoformat() if isinstance(obj, datetime.datetime) else None)
+
+def lambda_handler(event, context):
+    logger.info("Event: " + str(event))
+    message = event['Records'][0]['Sns']['Message']
+    logger.info("Message: " + str(message))
+
+    slack_message = {
+        'channel': SLACK_CHANNEL,
+        'text': "Message from AWS:\n%s" % (message)
+    }
+
+    req = Request(HOOK_URL, json.dumps(slack_message))
+    try:
+        response = urlopen(req)
+        response.read()
+        logger.info("Message posted to %s", slack_message['channel'])
+    except HTTPError as e:
+        logger.error("Request failed: %d %s", e.code, e.reason)
+    except URLError as e:
+        logger.error("Server connection failed: %s", e.reason)
